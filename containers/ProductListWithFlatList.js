78da5eb67e5007203809ea8b24a4a1bb34e18ae8
Narendra Babu Ghanta <narendrababu.ghanta@photoninfotech.net>
2018-09-12 12:34:49 +0530

    -Initial commit.

---
+++ containers/ProductListWithFlatList.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import ProductListItem from "../components/ProductListItem";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+  ActivityIndicator,
+  FlatList,
+  View
+} from "react-native";
+import * as productActionCreators from "../actionCreators/product";
+let URI = "http://localhost:4000";
+
+class ProductListWithFlatList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.actions.getProducts(this.props.page, this.props.limit);
+  }
+
+ _getProducts = (page = 1, limit = 40) => {
+    this.props.actions.getProducts(page, limit);
+  };
+
+  _getMore = () => {
+    this._getProducts(++this.props.page, this.props.limit);
+  };
+
+  _renderItem = ({ index, item }) => {
+    return (
+      <ProductListItem
+        {...this.props}
+        id={item.id}
+        title={`${item.id} - ${item.title}`}
+        image={item.image ? `${URI}/images/${item.image}` : null}
+        rating={item.rating}
+        price={item.price}
+       
+      />
+    );
+  };
+
+  _keyExtractor = (item, index) => {
+    return `${index}`;
+  };
+
+  render() {
+    this.props.products.sort(function(a,b){
+      return b.price > a.price;
+    })
+    return (
+      <View style={{flex:1,backgroundColor:'#fff'}}>
+        {this.props.isLoading ? (
+          <ActivityIndicator size="large" color="green" />
+        ) : (
+          <FlatList
+            data={this.props.products}
+            renderItem={this._renderItem}
+            keyExtractor={this._keyExtractor}
+            onEndReachedThreshold={0.5}
+            onEndReached={this._getMore}
+           
+          />
+        )}
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    products: state.productState.products,
+    isLoading: state.productState.isLoading,
+    page: state.productState.page,
+    limit: state.productState.limit
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(productActionCreators, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  ProductListWithFlatList
+);
